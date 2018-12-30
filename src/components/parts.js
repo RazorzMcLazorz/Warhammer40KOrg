48a82846b2f3351106f8cee3c8235312abbdbb0b
NathanielYoung <young.nathaniel95@gmail.com>
2018-12-29 23:16:55 -0700

    skitarii pics

--- src/components/parts.js
+++ src/components/parts.js
@@ -10,7 +10,30 @@ class Parts extends Component {
     return (
       <div id='parts'>
         <Nav/>
-        
+        <div id="skitariiStartCollectingBox">
+
+          <div id="scbShown">
+            Skitarii Start Collecting Box
+            <img id="scbp" src="./assets/SkitariiStartCollecting.jpg" />
+          </div>
+
+          <div id="scbHidden">
+            <img className="scbgrid" src="./assets/w40k1.jpg" />
+            <img className="scbgrid" src="./assets/w40k2.jpg" />
+            <img className="scbgrid" src="./assets/w40k3.jpg" />
+            <img className="scbgrid" src="./assets/w40k4.jpg" />
+            <img className="scbgrid" src="./assets/w40k5.jpg" />
+            <img className="scbgrid" src="./assets/w40k6.jpg" />
+            <img className="scbgrid" src="./assets/w40k7.jpg" />
+            <img className="scbgrid" src="./assets/w40k8.jpg" />
+            <img className="scbgrid" src="./assets/w40k9.jpg" />
+            <img className="scbgrid" src="./assets/w40k10.jpg" />
+            <img className="scbgrid" src="./assets/w40k11.jpg" />
+            <img className="scbgrid" src="./assets/w40k12.jpg" />
+            <img className="scbgrid" src="./assets/w40k13.jpg" />
+          </div>
+          
+        </div>
         <Footer/>
       </div>
     );
